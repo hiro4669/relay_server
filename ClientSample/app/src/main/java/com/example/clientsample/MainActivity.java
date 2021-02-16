accbe6d2e48f4a96b2f44c5eb753839043c3a855
Uta Hidai <50857020+Uta124@users.noreply.github.com>
2021-02-16 13:58:07 +0900

    Update MainActivity.java

--- ClientSample/app/src/main/java/com/example/clientsample/MainActivity.java
+++ ClientSample/app/src/main/java/com/example/clientsample/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
