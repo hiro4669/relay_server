63cdbb5f4ec3cbd1b744e77fc1dc3831a7fc5409
toriumi <ken.tennis914@gmail.com>
2021-02-26 17:57:10 +0900

    add MySQLSample

--- MySQLSample/show_databases.py
+++ MySQLSample/show_databases.py
@@ -2,22 +2,26 @@ import mysql.connector as mydb
 
 
 def show_databases():
-    # コネクションの作成
+    '''
+    This is a simple program that connects to mysql and executes sql "show databases" to list the databases.
+    '''
     connector = mydb.connect(
         host='localhost',
         port='3306',
-        user='root',
-        password='',
+        user='', # Your team's user name
+        password='', # Your team's password
         database=''
     )
 
-    print('connected??: {}'.format(connector.is_connected()))
+    print('connected?: {}'.format(connector.is_connected()))
     cursor = connector.cursor()
     show_databases_sql = "show databases"
     cursor.execute(show_databases_sql)
+
+    print('{}:'.format(show_databases_sql))
     for (show_databases_sql) in cursor:
         print(show_databases_sql[0])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     show_databases()
